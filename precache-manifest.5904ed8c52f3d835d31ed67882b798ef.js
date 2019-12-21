f7c30d54d840b9b9d8ff3de679e1bb84d776d7cf
Oprea Anca <opreararesstefan@gmail.com>
2019-12-21 22:53:56 +0100

    Updates

---
+++ precache-manifest.5904ed8c52f3d835d31ed67882b798ef.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b355379cb5750edf9a86bf124a97e17",
+    "url": "/opreaweb/index.html"
+  },
+  {
+    "revision": "0183942e5be35b5f6299",
+    "url": "/opreaweb/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "94cbadbdb78228941794",
+    "url": "/opreaweb/static/css/main.f20be842.chunk.css"
+  },
+  {
+    "revision": "0183942e5be35b5f6299",
+    "url": "/opreaweb/static/js/2.527ea853.chunk.js"
+  },
+  {
+    "revision": "eff37a317864acf53b3f487454b20ab3",
+    "url": "/opreaweb/static/js/2.527ea853.chunk.js.LICENSE"
+  },
+  {
+    "revision": "94cbadbdb78228941794",
+    "url": "/opreaweb/static/js/main.7d00ee2c.chunk.js"
+  },
+  {
+    "revision": "fcf0617338b75bf4c572",
+    "url": "/opreaweb/static/js/runtime-main.e08cf012.js"
+  },
+  {
+    "revision": "1b9125724c9b81dbd273bca702034d71",
+    "url": "/opreaweb/static/media/pozacontact.1b912572.jpeg"
+  },
+  {
+    "revision": "90da9bf48a0e148c1cef73589184c9e7",
+    "url": "/opreaweb/static/media/pozacv.90da9bf4.jpg"
+  }
+]);
